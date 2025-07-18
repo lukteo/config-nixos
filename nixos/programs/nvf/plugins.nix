bd504cd19e1c1df306103baae5ed72ac502d7724
Luke Teo <67887422+lukteo@users.noreply.github.com.>
2025-07-18 22:53:29 +0900

    fix updated oil to include more config for hidden files and navigation

--- nixos/programs/nvf/plugins.nix
+++ nixos/programs/nvf/plugins.nix
@@ -9,6 +9,22 @@ _: {
     utility = {
       oil-nvim = {
         enable = true;
+        setupOpts = {
+          columns = ["icon"];
+
+          view_options = {
+            show_hidden = true;
+            natural_order = true;
+          };
+
+          skip_confirm_for_simple_edits = true;
+          prompt_save_on_select_new_entry = true;
+          delete_to_trash = true;
+          constrain_cursor = "editable";
+          default_file_explorer = true;
+          watch_for_changes = true;
+          use_default_keymaps = true;
+        };
       };
     };
     terminal = {
